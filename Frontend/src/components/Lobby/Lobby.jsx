fa33d89aaba9f5575bbb69e3c1cff5aab24d8482
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-08-08 21:47:48 -0600

    Metodos luego de presionar botones

--- Frontend/src/components/Lobby/Lobby.jsx
+++ Frontend/src/components/Lobby/Lobby.jsx
@@ -4,18 +4,25 @@ import { Button } from 'rsuite'
 import { Modal } from 'rsuite'
 import { InputNumber } from 'rsuite'
 
+// import { useHistory } from "react-router-dom"
+
 import './style.scss' 
 
 const numberInput = { width: '50%' };
+// const history = useHistory()
 export default class Lobby extends React.Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
-			show: false
+			show: false,
+			codigoSala: ''
 		}
 		this.close = this.close.bind(this)
 		this.open = this.open.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+		this.sendToServer = this.sendToServer.bind(this)
+		this.crearSala = this.crearSala.bind(this)
 	}
 
 	close () {
@@ -30,12 +37,30 @@ export default class Lobby extends React.Component {
 		})
 	}
 
+	handleChange (value) {
+		this.setState({
+			codigoSala: value
+		})
+	}
+
+	sendToServer () {
+		console.log(this.state.codigoSala)
+		this.setState({
+			show: false
+		})
+		this.props.history.push('/game')
+	}
+
+	crearSala () {
+		this.props.history.push('/game');
+	}
+
 	render(){
 		return (
 			<div className='background-wood'>
 				<h1 className='title-lobby'>Lobby</h1>
 				<div className='order-components'>
-					<Button size="lg" color='cyan' block>Crear sala</Button>
+					<Button size="lg" color='cyan' block onClick={this.crearSala}>Crear sala</Button>
 					<Button size="lg" color='green' block onClick={this.open}>Unirse a sala</Button>
 				</div>
 
@@ -44,17 +69,19 @@ export default class Lobby extends React.Component {
 						<Modal.Title>Ingresa el código de la sala</Modal.Title>
 					</Modal.Header>
 					<Modal.Body>
-						<div style={{ textAlign: 'center' }}>
-							<InputNumber style={numberInput} defaultValue={100000} min={0} max={999999} placeholder='Código de sala'/>
+						<div style={{ display: 'flex', justifyContent: 'center' }}>
+							<InputNumber style={numberInput} min={0} placeholder='Código de sala' onChange={this.handleChange} />
 						</div>
 					</Modal.Body>
 					<Modal.Footer>
-						<Button onClick={this.close} appearance="primary">
-							Ok
-						</Button>
-						<Button onClick={this.close} appearance="subtle">
-							Cancel
-						</Button>
+						<div className='modal-buttons'>
+							<Button onClick={this.close} color="red">
+								Cancel
+							</Button>
+							<Button onClick={this.sendToServer} color="green">
+								Ok
+							</Button>
+						</div>
 					</Modal.Footer>
 				</Modal>
 			</div>
