d5819047f274110e006f327cb1e510643d54c7d1
Gustavo de Leon <gdeleontobias@gmail.com>
2020-08-08 21:26:12 -0600

    uri si se puedeeeee

--- Frontend/src/components/Lobby/Lobby.jsx
+++ Frontend/src/components/Lobby/Lobby.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import './style.scss' 
 import { Button } from 'rsuite'
 import { Modal } from 'rsuite'
 import { InputNumber } from 'rsuite'
@@ -8,6 +9,31 @@ import './style.scss'
 
 const numberInput = { width: '50%' };
 export default class Lobby extends React.Component {
+	oka() {
+		const client = new W3CWebSocket('ws://localhost:4200/', 'echo-protocol');
+		console.log('Se hizo click');
+			client.onopen = () => {
+				console.log('WebSocket Client Connected');
+				function sendNumber() {
+					if (client.readyState === client.OPEN) {
+						var number = Math.round(Math.random() * 0xFFFFFF);
+						client.send(number.toString());
+						setTimeout(sendNumber, 1000);
+					}
+				}
+				sendNumber();
+		  };
+		  client.onclose = function() {
+			console.log('echo-protocol Client Closed');
+			};
+			
+			client.onmessage = function(e) {
+				if (typeof e.data === 'string') {
+					console.log("Del server: '" + e.data + "'");
+				}
+			};
+	  }
+
 	constructor(props) {
 		super(props)
 
@@ -22,12 +48,14 @@ export default class Lobby extends React.Component {
 		this.setState({
 			show: false
 		})
+		
 	}
 
 	open () {
 		this.setState({
 			show: true
 		})
+		this.oka()
 	}
 
 	render(){
@@ -57,7 +85,7 @@ export default class Lobby extends React.Component {
 						</Button>
 					</Modal.Footer>
 				</Modal>
-			</div>
-		)
-	}
+				</div>
+			)
+		}
 }
