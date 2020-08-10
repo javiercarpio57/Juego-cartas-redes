2d73529463ca50d4e64f146872b9ac44369b7d79
Gustavo de Leon <gdeleontobias@gmail.com>
2020-08-10 15:46:07 -0600

    ya manda mensajes del cliente al server shuffle no terminado

--- server/index.js
+++ server/index.js
@@ -102,60 +102,16 @@ function crearSala(){
                     let card = stack.pop()
                     console.log(i+" | "+card)
                 }
+            }else
+            if(entradaCliente[0].localeCompare("PRUEBA")==0){
+                console.log("funcionaaaaaaaaaaaaaa")
             }
         });
         connection.on('close', function(reasonCode, description) {
             console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
         });
     });
-    cont++;
-}
-
-function unirseSala(){
-    let turno = cont;
-    servidores[turno] = http.createServer(function(request, response) {
-        console.log((new Date()) + ' Received request for ' + request.url);
-        response.writeHead(404);
-        response.end();
-    });
-    let puerto = PORT + turno
-    servidores[turno].listen(puerto,function() {
-        console.log((new Date()) + ' Server is listening on port '+puerto.toString());
-    });
-    sockets[turno] = new WebSocketServer({
-        httpServer: servidores[turno],
-        // You should not use autoAcceptConnections for production
-        // applications, as it defeats all standard cross-origin protection
-        // facilities built into the protocol and the browser.  You should
-        // *always* verify the connection's origin and decide whether or not
-        // to accept it.
-        autoAcceptConnections: false
-    });
-    sockets[turno].on('request', function(request) {
-        if (!originIsAllowed(request.origin)) {
-          // Make sure we only accept requests from an allowed origin
-          request.reject();
-          console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-          return;
-        }
-        
-        var connection = request.accept('echo-protocol', request.origin);
-        console.log((new Date()) + ' Connection accepted.');
-        connection.on('message', function(message) {
-            if (message.type === 'utf8') {
-                console.log(turno.toString()+' Received Message de server : ' + message.utf8Data);
-                connection.sendUTF(message.utf8Data);
-            }
-            else if (message.type === 'binary') {
-                console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-                connection.sendBytes(message.binaryData);
-            }
-        });
-        connection.on('close', function(reasonCode, description) {
-            console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        });
-    });
-    cont++;
+    //cont++;
 }
 
 
