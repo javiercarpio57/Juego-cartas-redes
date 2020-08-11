2f4a1319d64530f77399328808a3fb53b8582a90
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-08-10 21:46:21 -0600

    Icono a input de usuario

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { Route, Link } from 'react-router-dom'
-import { Button, Modal, InputNumber, Input } from 'rsuite'
+import { Button, Modal, InputNumber, Input, InputGroup, Icon } from 'rsuite'
 
 import './style.scss'
 import 'rsuite/dist/styles/rsuite-default.css'
@@ -32,11 +32,23 @@ export default class App extends React.Component {
     }
 
     render () {
+        const styles = {
+            height: 52
+        };
+
         return (
             <div className= 'main'>
                 <div className= 'inputArea'>
                     <div className= 'inputField'>
-                        <Input style={{ height:80, textAlign:"center", fontSize:30, width: 400 }}  min={0} placeholder="Username" onChange={this.handleChange}/> 
+                        <InputGroup style={styles}>
+                            <InputGroup.Addon>
+                                <Icon icon="avatar" />
+                            </InputGroup.Addon>
+                            <Input style={{ height: 50, textAlign:"center", fontSize:30, width: 400 }}
+                                placeholder="Ingresa tu usuario"
+                                onChange={this.handleChange}
+                            /> 
+                        </InputGroup>
                     </div>
                 </div>
                 <div className= 'buttonArea'>                    
