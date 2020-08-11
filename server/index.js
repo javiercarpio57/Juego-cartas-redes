09623c476920ca60f6fdd14d640755f61afce5b6
Andres Urizar <andresum1309@gmail.com>
2020-08-11 14:50:15 -0600

    formato de server cambiado

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
@@ -8,120 +9,113 @@ let sockets = []
 let usuarios = {}
 let PORT = 4200
 const cards = [
-	'guard','guard','guard','guard',
-	'guard','priest','priest','baron',
-	'baron','handmaid', 'handmaid','prince',
-	'prince','king','countess','princess'
-    ]
+    'guard', 'guard', 'guard', 'guard',
+    'guard', 'priest', 'priest', 'baron',
+    'baron', 'handmaid', 'handmaid', 'prince',
+    'prince', 'king', 'countess', 'princess'
+]
 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
+
 function originIsAllowed(origin) {
     // put logic here to detect whether the specified origin is allowed.
     return true;
-  }
-function crearSala(){
-    console.log("He llegado de nuevo a crear sala")
+}
+
+function crearSala() {
+    //============== Variables de sala generales 
     let usuariosIngresados = 0;
     let turno = cont;
     let puerto = PORT + turno
     let cartaServer = cards
     let socketsClients = []
     usuarios[puerto] = {}
+    //=====================================
+    //============== Revolver a cartas 
     shuffle(cartaServer)
-    let stack = []; 
+    let stack = [];
     let personas = 'usuarios|'
     let repartoInicial = 'cartas|'
-    cartaServer.forEach(carta=>stack.push(carta))
-    servidores[turno] = http.createServer(function(request, response) {
+    cartaServer.forEach(carta => stack.push(carta))
+    //========================================
+    //============ Creacion de nuevo socket para sala
+    servidores[turno] = http.createServer(function (request, response) {
         console.log((new Date()) + ' Received request for ' + request.url);
         response.writeHead(404);
         response.end();
     });
-    servidores[turno].listen(puerto,function() {
-        console.log((new Date()) + ' Server is listening on port '+puerto.toString());
+    servidores[turno].listen(puerto, function () {
+        console.log((new Date()) + ' Server is listening on port ' + puerto.toString());
     });
     sockets[turno] = new WebSocketServer({
         httpServer: servidores[turno],
         autoAcceptConnections: false
     });
-    sockets[turno].on('request', function(request) {
+    //=================================================
+    //=========== Conexion de socket general => Aqui se desarrolla el intercambio servidor cliente
+    sockets[turno].on('request', function (request) {
         if (!originIsAllowed(request.origin)) {
-          // Make sure we only accept requests from an allowed origin
-          request.reject(); 
-          console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-          return;
+            request.reject();
+            console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+            return;
         }
         var connection = request.accept('echo-protocol', request.origin);
         socketsClients.push(connection);
         console.log((new Date()) + ' Connection accepted.');
 
-        connection.on('message', function(message) {
-            console.log("El mensaje ingresado es "+message.utf8Data);
+        connection.on('message', function (message) {
+            console.log("El mensaje ingresado es " + message.utf8Data);
             let mensaje = message.utf8Data
             let entradaCliente = mensaje.split("|");
-            // instruccion | username | target 
-            if(entradaCliente[0].localeCompare("dondeConecto")==0){
-                console.log("entro a preguntar y el valor de cont es: "+cont);
+
+            //====== Formato de envio de juego   => instruccion | username | target 
+            if (entradaCliente[0].localeCompare("dondeConecto") == 0) {
+                console.log("entro a preguntar y el valor de cont es: " + cont);
                 cont++;
-                nuevopuerto = PORT+cont;
+                nuevopuerto = PORT + cont;
                 connection.sendUTF(nuevopuerto.toString());
                 crearSala();
-                
-            }else
-            if(entradaCliente[0].localeCompare("conectarmeASala")==0){
-                usuariosIngresados++;
 
-                personas = personas+usuariosIngresados+"|"+entradaCliente[1]+"|"
-                
-                console.log("Me he conectado exitosamente "+":"+puerto);
-                
-                console.log("personas conectadas "+personas)
+            } else
+            if (entradaCliente[0].localeCompare("conectarmeASala") == 0) {
                 let temp = {}
+                usuariosIngresados++;
+                personas = personas + usuariosIngresados + "|" + entradaCliente[1] + "|"
+
+                console.log("Me he conectado exitosamente " + ":" + puerto);
+
                 temp = {
                     "username": entradaCliente[1],
                     "cartas": []
                 }
 
-                console.log("Mira temp"+JSON.stringify(temp));
-                console.log("Esto es usuarios"+JSON.stringify(usuarios));
                 usuarios[puerto][usuariosIngresados] = temp;
                 console.log(usuarios)
-                let mensaje = "conectado|"+usuariosIngresados
+                let mensaje = "conectado|" + usuariosIngresados
                 connection.sendUTF(mensaje);
 
                 let card = stack.pop();
                 let indice = usuariosIngresados.toString();
-                console.log("inice"+indice);
+
                 usuarios[puerto][indice]["cartas"].push(card)
-                console.log("Esto esta usuarios"+JSON.stringify(usuarios));
-                
-                repartoInicial = repartoInicial+usuariosIngresados+"|"+card+"|"
-
-                socketsClients.forEach(function(client){
+                repartoInicial = repartoInicial + usuariosIngresados + "|" + card + "|"
+                socketsClients.forEach(function (client) {
                     client.sendUTF(personas);
-                    //client.sendUTF(repartoInicial);
                 })
-
-                
-
-            }else
-            if(entradaCliente[0].localeCompare("iniciar")==0){
-                console.log("entro al if")
-                console.log(repartoInicial)
-                socketsClients.forEach(function(client){
+            } else
+            if (entradaCliente[0].localeCompare("iniciar") == 0) {
+                socketsClients.forEach(function (client) {
                     client.sendUTF(repartoInicial);
                 })
             }
         });
-        connection.on('close', function(reasonCode, description) {
+        connection.on('close', function (reasonCode, description) {
             console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
         });
     });
-    //cont++;
 }
 
 
 crearSala();
- 
