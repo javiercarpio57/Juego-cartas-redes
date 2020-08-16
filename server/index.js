97670dcc45fd404fa18f43f8bcac0c5d86289c0b
Andres Urizar <andresum1309@gmail.com>
2020-08-16 13:21:01 -0600

    ya corregido salto de turno en sin efecto

--- server/index.js
+++ server/index.js
@@ -596,7 +596,32 @@ function crearSala() {
             let numTuyo = getKeyByValue(usuarios[puerto],efectoTu)
 
             let idSinEfecto = usuarios[puerto][numTuyo]["cartas"].indexOf(efectoCarta);
-            turnoJugador++;
+            let siguienteJugador = turnoJugador+1;
+            if(siguienteJugador > 4){
+                siguienteJugador = 1;
+            }
+            //Esto es para cambiar de turno y asignar una carta al siguiente usuario
+            if( usuarios[puerto][siguienteJugador]["vivo"] == true){
+                turnoJugador++;
+                if(turnoJugador > 4){
+                    turnoJugador = 1;
+                }
+            }else
+            if(usuarios[puerto][siguienteJugador]["vivo"] == false){
+                turnoJugador += 2;
+                if(turnoJugador == 6){
+                    turnoJugador = 2;
+                }else
+                if(turnoJugador == 5){
+                    turnoJugador = 1;
+                }
+                if(usuarios[puerto][turnoJugador]["vivo"] == false){
+                    turnoJugador++;
+                    if(turnoJugador > 4){
+                        turnoJugador = 1;
+                    }
+                }
+            }
             socketsClients.forEach(function (client) {
                 client.sendUTF("sinEfectoCliente|"+efectoTu+"|"+efectoCarta)
             })
